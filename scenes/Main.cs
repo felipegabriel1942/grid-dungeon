8bae7f7ed2018e4ff4318fb8d6b72ae68fe4a734
FelipePinheiro <pinheiro_felipeg@yahoo.com.br>
2025-08-23 13:27:31 -0300

    Capturando posição do mouse no grid

--- scenes/Main.cs
+++ scenes/Main.cs
@@ -1,6 +1,24 @@
+using Game.Manager;
 using Godot;
 using System;
 
+namespace Game;
+
 public partial class Main : Node
 {
+
+	private GridManager gridManager;
+
+	public override void _Ready()
+	{
+		gridManager = GetNode<GridManager>("GridManager");
+	}
+
+	public override void _Process(double delta)
+	{
+		var mouseGridPosition = gridManager.GetMouseGridCellPosition();
+		GD.Print(mouseGridPosition);
+	}
+
+
 }
--- scenes/manager/GridManager.cs
+++ scenes/manager/GridManager.cs
@@ -1,27 +1,34 @@
 using Godot;
-using System;
 
 namespace Game.Manager;
 
 public partial class GridManager : Node
 {
 
-    [Export]
-    private TileMapLayer baseTerrainTileMapLayer;
+	[Export]
+	private TileMapLayer baseTerrainTileMapLayer;
 
-    public override void _Ready()
-    {
-        GD.Print(baseTerrainTileMapLayer.GetChildren().Count);
+	[Export]
+	private TileMapLayer highlightTileMapLayer;
 
-        
-    }
+	public override void _Ready()
+	{
+		GD.Print(baseTerrainTileMapLayer.GetChildren().Count);
+	}
+
+	public Vector2I GetMouseGridCellPosition()
+	{
+		var mousePosition = highlightTileMapLayer.GetGlobalMousePosition();
+		var gridPosition = mousePosition / 16;
+		gridPosition = gridPosition.Floor();
+		return new Vector2I((int) gridPosition.X, (int) gridPosition.Y);
+	}
+
+	public bool TileHasCustomData(Vector2I tilePosition, string dataName)
+	{
 
 
-    public bool TileHasCustomData(Vector2I tilePosition, string dataName)
-    {
-
-
-        return false;
-    }
+		return false;
+	}
 
 }
